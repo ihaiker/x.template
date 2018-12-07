3e6c3a7883f124500092fcefba1a032cdf0347f5
haiker <zhouhaichao@2008.sina.com>
2018-12-07 22:18:43 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # x.template
-gulp template plguins 模板
+gulp template plugins 一个优雅的模板
+
+
+## Installation
+
+```shell
+$ npm install x.template --save
+```
+
+## Usage
+
+```javascript
+var gulp = require('gulp');
+gulp.import = require('x.template');
+```
+
+
+## Example
+In your `gulpfile.js`:
+```
+var gulp = require('gulp'),
+xImport = require('x.import');
+
+gulp.task('html', function () {
+    gulp.src(['./src/**/*.html', '!./src/layouts/*.html'])
+        .pipe(template({ "template":"./src/layouts/template.html"}, {"title": "An elegant template"}))
+        .pipe(gulp.dest('build'));
+});
+
+```
+
+In your `./src/layouts/template.html`
+```html
+<html>
+<head>
+    <title><%=title%></title>
+    <%=header%>
+</head>
+<body>
+    <div class="content">
+    <%=body%>
+    </div>
+
+    <%=scripts%>
+</body>
+</html>
+```
+
+In your `*.html`
+
+`There is no example here, just follow the usual way.`
+
+## License
+Apache License 2.0
---
+++ index.js
@@ -0,0 +1,73 @@
+var evs = require('event-stream'),
+    gutil = require('gulp-util'),
+    defaults = require('defaults'),
+    artTemplate = require("art-template"),
+    processDir = require("process").cwd(),
+    path = require('path'),
+    fs = require("fs"),
+    PluginError = gutil.PluginError;
+
+var jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+const {window} = new JSDOM();
+var $ = jQuery = require('jquery')(window);
+
+var pattern = /([<][!][-]{2}).?layout[(]?.?["'](.*)["'].?[)]?.?[-]{2}[>]/g;
+
+function exportData(file) {
+    var {document} = (new JSDOM(String(fs.readFileSync(file.path)))).window;
+    global.document = document;
+
+    var title = document.title;
+    var scripts = $(document).find("head script").prop("outerHTML");
+
+    $(document).find("head title,head script").remove();
+
+    var header = $(document).find("head").html();
+    var body = $(document).find("body");
+    return {
+        title: title, header: header, body: body.html(), scripts: scripts
+    };
+}
+
+function getTemplateLayout(file) {
+    var array = fs.readFileSync(file.path).toString().split("\n");
+    var match = pattern.exec(array[0]);
+    if (match) {
+        return path.join(path.dirname(file.path),match[2]);
+    }
+    return null;
+}
+
+/**
+ * @param options 配置文件 查阅artTemplate配置
+ * @param defaultDatas 默认数据
+ * @returns {*}
+ */
+function gulpElegantTemplate(options, defaultDatas) {
+    options = defaults({
+        "root": processDir,
+        "tempalte": "./src/layouts/template.html"
+    }, options);
+
+    artTemplate.defaults.escape = false;
+    artTemplate.defaults.root = options.root;
+
+    function process(file) {
+        if (file.isStream()) {
+            this.emit('error', new PluginError("gulp-x-template", 'Streams aren\'t supported'));
+        }
+
+        if (file.isBuffer()) {
+            var data = exportData(file);
+            var templateFile = getTemplateLayout(file) || options.tempalte;
+            var content = artTemplate(templateFile, defaults(data, defaultDatas));
+            file.contents = new Buffer(content);
+        }
+        this.emit('data', file);
+    }
+
+    return evs.through(process);
+}
+
+module.exports = gulpElegantTemplate;
